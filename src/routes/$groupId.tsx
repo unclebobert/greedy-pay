a8bbdd5f555dee4249e3ba27146e3534ca2d5a74
unclebobert <jacksonfu777@gmail.com>
2025-09-05 23:47:16 +1000

    fix some formatting on mobile

--- src/routes/$groupId.tsx
+++ src/routes/$groupId.tsx
@@ -15,7 +15,7 @@ function Group() {
   const groupId = parseInt(groupIdRaw)
   const group = useLiveQuery(() => db.groups.get(groupId), [groupId])
   return (
-    <div className='w-full h-full px-16 py-12'>
+    <div className='w-full h-full p-8 md:px-16 md:py-12'>
       {group ?
         <GroupView group={group} /> :
         <LoaderCircle className='animate-spin m-auto size-24' />
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,7 +9,7 @@ export const Route = createFileRoute('/')({
 function Index() {
   return (
     <div className='w-full min-h-full flex flex-col justify-center items-center gap-8'>
-      <h1 className='text-3xl font-semibold'>
+      <h1 className='mx-2 text-3xl font-semibold text-center'>
         Create a new group to split bills better
       </h1>
       <NewGroupButton className='size-16' />
